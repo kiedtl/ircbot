5922eb0f78817598896126ea62f498270ce6fa9a
Kiëd Llaentenn <kiedtl@tilde.team>
2020-06-04 18:29:36 -0400

    fortune: update

--- modules/fortune.py
+++ modules/fortune.py
@@ -2,11 +2,11 @@ import subprocess
 from subprocess import Popen, PIPE, STDOUT
 
 async def fortune(self, c, n, m):
-    proc = Popen(["fortune"],
+    proc = Popen(["fortune", "-s", "-e"],
             stdout=PIPE, stderr=STDOUT)
     (out, err) = proc.communicate()
     exit = proc.wait()
-    fort = out.decode('utf-8')
+    fort = out.decode('utf-8').rstrip()
     await self.message(c, fort)
 
 async def init(self):

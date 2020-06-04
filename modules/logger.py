7e77c367677a7097a7dd8364747175b391b61ef5
Kiëd Llaentenn <kiedtl@tilde.team>
2020-06-04 19:10:42 -0400

    fix logger

--- modules/logger.py
+++ modules/logger.py
@@ -3,20 +3,11 @@ import datetime, random
 async def logger(self, chan, src, msg):
     date = datetime.datetime.now()
     time = datetime.datetime.strftime(date, '%d%m%y%H%M')
-
-    if chan not in self.log:
-        self.log[chan] = []
-
-    self.log[chan].append([time, src, msg])
-
-    if len(self.log[chan]) > 512:
-        logf = open('chans/{}.log'.format(chan), 'a')
-        for i in self.log[chan]:
-            logf.write('{} {} {}\n'
-                    .format(i[0], i[1], i[2]))
-        logf.close()
-        self.log[chan] = []
+    if chan not in self.logfiles:
+        self.logfiles[chan] = open('chans/{}.log'
+            .format(chan), 'a')
+    self.logfiles[chan].write('{} {} {}\n'.format(time, src, msg))
 
 async def init(self):
-    self.log = {}
+    self.logfiles = {}
     self.raw['logger'] = logger

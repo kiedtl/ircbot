4fe649263554b0daee9b4dd911dd48acb2aade27
Kiëd Llaentenn <kiedtl@tilde.team>
2020-06-05 16:50:30 -0400

    misc: add misc modules

---
+++ modules/misc.py
@@ -0,0 +1,14 @@
+# random commands that involve
+# simply piping text into a command
+# and posting the result
+
+import subprocess
+from subprocess import Popen, PIPE, STDOUT
+
+async def list_mods(self, chan, src, msg):
+    await self.message(chan, '[modules] loaded: {}'
+        .format(list(self.modules.keys())))
+
+async def init(self):
+    self.cmd['modules'] = list_mods
+    self.help['modules'] = ['modules - list loaded modules', '']

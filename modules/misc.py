b9f9a3fb76ef51e2b609d08f708516bd5bb07788
Kiëd Llaentenn <kiedtl@tilde.team>
2020-06-06 13:13:59 -0400

    add ping command

--- modules/misc.py
+++ modules/misc.py
@@ -2,14 +2,21 @@
 # simply piping text into a command
 # and posting the result
 
-import subprocess, common
-from common     import modname
-from subprocess import Popen, PIPE, STDOUT
+import subprocess, common, random
+from common import modname
 
 async def list_mods(self, chan, src, msg):
     await self.message(chan, '{} loaded: {}'
         .format(modname('modules'), list(self.modules.keys())))
 
+async def ping(self, chan, src, msg):
+    await self.message(chan, '{} {}: {}'
+        .format(modname('ping'), src,
+            random.choice(['yes?', 'pong!', 'what?', 'hmmm?',
+                'at your service!'])))
+
 async def init(self):
     self.cmd['modules'] = list_mods
+    self.cmd['ping'] = ping
     self.help['modules'] = ['modules - list loaded modules']
+    self.help['ping'] = ['ping - check if I\'m responding']

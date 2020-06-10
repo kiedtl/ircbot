6199b7b413c92f1696c870993bfc2c76a072d363
Kiëd Llaentenn <kiedtl@tilde.team>
2020-06-10 11:24:24 -0400

    misc:

--- modules/chanstats.py
+++ modules/chanstats.py
@@ -22,9 +22,37 @@ async def totalmsgs(self, chan, src, msg):
     await self.message(chan, '{} {:,} total messages for {} channel(s)'
             .format(modname(module_name), len(logs), channels))
 
+async def totalnicks(self, chan, src, msg):
+    channels = 'all'
+    logfiles = []
+    for i in [s for s in os.listdir('chans') if '#' in s]:
+        logfiles.append(i)
+    if len(msg) > 0:
+        channels = ', '.join(msg.split(' '))
+        logfiles = msg.split(' ')
+
+    stats = {}
+
+    for logfile in logfiles:
+        logs = []
+        try:
+            logs = loadlogs(chan)
+        except:
+            await self.message('{} error opening log file'
+                .format(modname(module_name)))
+            return
+        for log in logs:
+            nick = log.split()[1]
+            print(f'log={log}, nick={nick}')
+            if not nick in stats:
+                stats[nick] = 0
+            stats[nick] += 1
+    await self.message(chan, '{} {:,} total unique nicks for {} channel(s)'
+            .format(modname(module_name), len(stats), channels))
 
 commands = {
-    'totalmsgs': totalmsgs
+    'totalmsgs': totalmsgs,
+    'totalnicks': totalnicks
 }
 
 async def chanstats_handle(self, chan, source, msg):

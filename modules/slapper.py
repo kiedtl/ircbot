49d9be1a202bbee2e8f6af25660d9526c44ad443
Kiëd Llaentenn <kiedtl@tilde.team>
2020-06-22 12:12:26 -0400

    slapper: add

---
+++ modules/slapper.py
@@ -0,0 +1,27 @@
+import common
+import time
+import re
+
+async def filterslap(self, chan, src, msg):
+    """detect if message slaps k"""
+    if self.is_slap.match(msg) and src != self.nickname:
+        print(f'[misc] slapped by {src} in {chan}')
+        if not src in self.slapped:
+            self.slapped[src] = 0
+        if not self.slapped[src] + 21600 < time.time():
+            return
+        else:
+            await self.message(chan, f';slap {src}')
+            self.slapped[src] = time.time()
+
+async def init(self):
+    self.raw['filterslap'] = filterslap
+
+    # keeps track of the last time a user
+    # was slapped
+    # if we slap the same user twice within
+    # the same 6 hour period, we lose points
+    # instead of getting them
+    self.slapped = {}
+
+    self.is_slap = re.compile('^;slap\ ' + self.nickname)

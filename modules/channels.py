822c923c35f01e80ede664204b01597e42bf556e
MineRobber___T <khuxkm@ttm.sh>
2020-06-22 21:37:56 -0400

    Remove meta from join list
    You don't have permission to put k in #meta, so stop trying to put k in #meta.

--- modules/channels.py
+++ modules/channels.py
@@ -2,7 +2,7 @@ async def init(self):
     # channels that are joined only when the
     # `:admin joins` command is issued
     self.joins = ['#team', '#lickthecheese', '#chaos',
-        '#unruly', '#meta']
+        '#unruly']
 
     # channels that are joined initially
     self.chansjoin = ['#bots', '#spacehare']

9c4fecd48bb8a6eac40a9717b62c1321a34e37f0
Kiëd Llaentenn <kiedtl@tilde.team>
2020-06-23 20:16:48 -0400

    Update channels.py

--- modules/channels.py
+++ modules/channels.py
@@ -1,8 +1,7 @@
 async def init(self):
     # channels that are joined only when the
     # `:admin joins` command is issued
-    self.joins = ['#team', '#lickthecheese', '#chaos',
-        '#unruly', '#meta']
+    self.joins = ['#team', '#lickthecheese', '#chaos', '#unruly']
 
     # channels that are joined initially
     self.chansjoin = ['#bots', '#spacehare']

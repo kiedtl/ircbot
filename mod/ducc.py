3ccd634331ac644ce6a4d6311f6507ba3400cca5
Kiëd Llaentenn <kiedtl@tilde.team>
2020-06-28 16:48:22 -0400

    ducc: add

---
+++ mod/ducc.py
@@ -0,0 +1,57 @@
+import dataset, out, random
+
+modname = 'ducc'
+
+async def ducc_cure(self, c, n, m):
+    """ cure the ducc (need admin privs) """
+    pass
+
+async def ducc_set_hp(self, c, n, m):
+    """ set the health level of ducc (need admin privs) """
+    pass
+
+async def ducc_set_stress(self, c, n, m):
+    """ set the stress level of ducc (need admin privs) """
+    pass
+
+async def ducc_feed(self, c, n, m):
+    """ feed the ducc and increase its health points """
+    pass
+
+async def ducc_pet(self, c, n, m):
+    """ pet the ducc and decrease its stress level """
+    pass
+
+async def ducc_quack(self, c, n, m):
+    """ QUACK! """
+    pass
+
+async def ducc_shoot(self, c, n, m):
+    """ try to kill the ducc :( """
+    pass
+
+async def ducc_info(self, c, n, m):
+    """ display health, stress, etc """
+    pass
+
+commands = {
+    'cure':       ducc_cure,
+    'set_hp':     ducc_set_hp,
+    'set_stress': ducc_set_stress,
+    'feed':       ducc_feed,
+    'pet':        ducc_pet,
+    'quack':      ducc_quack,
+    'shoot':      ducc_shoot,
+    'info':       ducc_info
+}
+
+async def ducc_handle(self, c, src, msg):
+    msg = msg.split(' ')
+    if len(msg) < 1 or not msg[0] in commands:
+        await out.msg(self, modname, c, [self.err_invalid_command])
+        return
+    await commands[msg.pop(0)](self, c, src, ' '.join(msg))
+
+async def init(self):
+    self.duccdb = dataset.connect('sqlite:///dat/ducc.db')
+    self.cmd['ducc'] = ducc_handle

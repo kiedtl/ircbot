123fb3dc40621df87fe6ed07f63e55c4e1393188
Kiëd Llaentenn <kiedtl@tilde.team>
2020-06-29 09:52:38 -0400

    txt: add some useless rubbish

--- mod/txt.py
+++ mod/txt.py
@@ -4,12 +4,24 @@
 
 import config, common, out
 
-async def figlet(self, chan, src, msg):
+async def cmd_with_args(self, chan, cmd, msg):
     # TODO: throttling, disable in certain channels
-    # TODO: fonts
-    res = common.run(self, ['figlet'], msg)
+    cmd = cmd + msg.split(' ')
+    res = common.run(self, cmd, msg)
     for line in res.split('\n'):
         await self.message(chan, line)
+
+async def figlet(self, chan, src, msg):
+    await cmd_with_args(self, chan, ['figlet'], msg)
+
+async def toilet(self, chan, src, msg):
+    await cmd_with_args(self, chan, ['toilet', '--irc'], msg)
+
+async def cowsay(self, chan, src, msg):
+    await cmd_with_args(self, chan, ['cowsay'], msg)
+
+async def cowthink(self, chan, src, msg):
+    await cmd_with_args(self, chan, ['cowthink'], msg)
 
 async def communist(self, chan, src, msg):
     txt = msg.upper()
@@ -40,9 +52,12 @@ async def rot13(self, chan, src, msg):
     await out.msg(self, 'rev13', chan, [f'<{txt[0]}> {res}'])
 
 async def init(self):
-    self.cmd['rev13']   = rev13
-    self.cmd['rot13']   = rot13
-    self.cmd['figlet']  = figlet
+    self.cmd['cowsay']    = cowsay
+    self.cmd['cowthink']  = cowthink
+    self.cmd['figlet']    = figlet
+    self.cmd['rev13']     = rev13
+    self.cmd['rot13']     = rot13
+    self.cmd['toilet']    = toilet
 
     self.cmd['communist'] = communist
     self.cmd['com']       = communist
@@ -51,4 +66,7 @@ async def init(self):
     self.help['rev13']   = ['rev13 - attempt to decrypt rot13-encrypted messages']
     self.help['communist'] = ['communist|com - \x038,5 ☭ SEIZE THE MEANS OF CHAOS PRODUCTION ☭\x04']
     self.help['com'] = self.help['communist']
-    self.help['figlet'] = ['figlet [text] - use /bin/figlet to generate ascii art']
+    self.help['figlet'] = ['figlet [args] - use /bin/figlet to generate ascii art']
+    self.help['toilet'] = ['toilet [args] - use /bin/toilet to generate ascii art']
+    self.help['cowsay'] = ['cow{say,think} [args] - use /bin/cow{say,think} to generate ascii art']
+    self.help['cowthink'] = self.help['cowsay']

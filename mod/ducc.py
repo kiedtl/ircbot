e2efe70dfe9c433c9c600f5b21f214828e3cdded
Kiëd Llaentenn <kiedtl@tilde.team>
2020-07-01 12:04:57 -0400

    ducc: implement!

--- mod/ducc.py
+++ mod/ducc.py
@@ -4,10 +4,14 @@ from babel.dates import format_timedelta
 
 modname = 'ducc'
 
+# TODO: create helper functions for decreasing or
+# increasing health or stress
+
 def ducc_hp_to_str(lvl):
     """ convert health points to string """
     hp_str = {
         # 0 == death
+        0:   'dying',
         7:   'dying',
         14:  'very ill',
         35:  'ill',
@@ -32,8 +36,8 @@ def ducc_sl_to_str(lvl):
         48: 'a bit stressed',
         56: 'very stress out',
         70: 'alarmed',
-        95: 'extremely stressed',
-        # 100 == death
+        95: 'extremely stressed out',
+        100: 'nothing',
     }
 
     while not lvl in sl_str:
@@ -42,7 +46,7 @@ def ducc_sl_to_str(lvl):
 
 async def ducc_update_state(self):
     """ update the duccs state """
-    cur_state = list(self.ducc_state.find())[-1]
+    cur_state = list(self.ducc_state)[-1]
     new_state = cur_state
 
     # check CUR_STATE, and see if the ducc was dead
@@ -71,7 +75,7 @@ async def ducc_update_state(self):
     # oh, and if the duccs health is 0, kill ducc.
     if cur_state['health'] < 45:
         hp_under_45 = 45 - cur_state['health']
-        for i in range(0, hp_under_45 / 10):
+        for i in range(0, int(hp_under_45 / 10)):
             new_state['stress'] += 15
 
     if cur_state['health'] < 1:
@@ -83,7 +87,9 @@ async def ducc_update_state(self):
         new_state['alive'] = False
 
     # insert NEW_STATE into db, after only after
-    # incrementing the id
+    # incrementing the id and checking health/stress
+    new_state['health'] = max(000, new_state['health'])
+    new_state['stress'] = min(100, new_state['stress'])
     new_state['id'] += 1
     self.ducc_state.insert(new_state)
 
@@ -92,10 +98,11 @@ async def ducc_cure(self, c, n, m):
     if not await self.is_admin(n):
         await out.msg(self, modname, c, [f'insufficient privileges'])
         return
-    last_state = list(self.ducc_state.find())[-1]
+    last_state = list(self.ducc_state)[-1]
     self.ducc_state.insert(
         dict(last_fed=int(datetime.datetime.now().strftime('%s')),
             health=100, stress=0, alive=True))
+    await out.msg(self, modname, c, [f'done'])
 
 async def ducc_feed(self, c, n, msg):
     """ feed the ducc and increase its health points """
@@ -125,12 +132,18 @@ async def ducc_feed(self, c, n, msg):
     for thing in m:
         inv.delete(id=its['id'])
 
+    state = list(self.ducc_state)[-1]
+    state['health'] += self.bakedGoods[thing] / 10
+    state['id'] += 1
+    self.ducc_state.insert(state)
     await out.msg(self, modname, c, [f'you fed the ducc!'])
-    # TODO: increase health points
 
 async def ducc_pet(self, c, n, m):
     """ pet the ducc and decrease its stress level """
-    # TODO: decrease stress
+    state = list(self.ducc_state)[-1]
+    state['stress'] -= 10
+    state['id'] += 1
+    self.ducc_state.insert(state)
     await ducc_quack(self, c, n, m)
 
 async def ducc_quack(self, c, n, m):
@@ -143,7 +156,11 @@ async def ducc_quack(self, c, n, m):
 
 async def ducc_shoot(self, c, n, m):
     """ try to kill the ducc :( """
-    # TODO: increase stress
+    state = list(self.ducc_state)[-1]
+    state['stress'] += 10
+    state['id'] += 1
+    self.ducc_state.insert(state)
+
     dodges = True
     if random.uniform(0, 1000) == 666:
         dodges = False
@@ -153,6 +170,7 @@ async def ducc_shoot(self, c, n, m):
         damage = (random.choices(
             population=[10, 30, 50, 70, 90],
             weights=[0.7, 0.2, 0.05, 0.03, 0.01]))[0]
+
         damages = {
             10: 'lightly wounded',
             30: 'moderately wounded',
@@ -160,14 +178,18 @@ async def ducc_shoot(self, c, n, m):
             70: 'severely wounded',
             90: 'almost dead'
         }
+
+        state = list(self.ducc_state)[-1]
+        state['health'] -= damage
+        state['id'] += 1
+        self.ducc_state.insert(state)
+
         await out.msg(self, modname, c,
             [f'you shot the ducc! the ducc is {damages[damage]}!'])
-        # TODO: remove health
-        # TODO: base damage str on total health after shot, not damage count
 
 async def ducc_info(self, c, n, m):
     """ display health, stress, etc """
-    state = list(self.ducc_state.find())[-1]
+    state = list(self.ducc_state)[-1]
 
     alive = bool(state['alive'])
     health = ducc_hp_to_str(state['health'])

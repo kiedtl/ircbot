cded13fd09af72863b95bf595e21cdb80a9b696d
Kiëd Llaentenn <kiedtl@tilde.team>
2020-07-06 17:51:59 -0400

    dat/pnts_init.sql: update

--- dat/pnts_init.sql
+++ dat/pnts_init.sql
@@ -1,15 +1,7 @@
 -- karma for each user
---
--- each karma point is stored by itself.
---
--- and yes, I know that storing each karma point
--- individually is not nearly as efficient as
--- storing it all together, but I think the speed
--- loss from doing it this way instead is
--- negotiable.
 CREATE TABLE karma (
         id       INTEGER NOT NULL,
         username TEXT    NOT NULL,
-        date     INTEGER NOT NULL,
+        amount   INTEGER NOT NULL,
         PRIMARY KEY (id)
 );

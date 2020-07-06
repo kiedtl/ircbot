e09460275e97039bc3e44a0688627b910d664d73
Kiëd Llaentenn <kiedtl@tilde.team>
2020-07-06 17:43:05 -0400

    url: title: add

--- mod/url.py
+++ mod/url.py
@@ -1,9 +1,40 @@
 # url-based functions
-# TODO: get title of url
-# TODO: unshorten url
 
-import nullptr, out
+import common, re, urllib, nullptr, out
+from bs4 import BeautifulSoup as BS
 modname = 'url'
+
+async def filterurl(self, chan, src, msg):
+    """
+    Detect a url in chat.
+    """
+    if self.url_re.match(msg):
+        try:
+            http = urllib.request.urlopen(msg)
+            data = BS(http)
+        except:
+            return
+        await out.msg(self, modname, chan, [data.title.string])
+
+async def title(self, chan, src, msg):
+    try:
+        txt = common.get_backlog_msg(self, chan, msg)[1]
+    except:
+        await out.msg(self, modname, chan,
+            [self.err_backlog_too_short])
+        return
+
+    if not txt.startswith('http'):
+        txt = 'http://' + txt
+
+    try:
+        http = urllib.request.urlopen(txt)
+        data = BS(http)
+    except:
+        await out.msg(self, modname, chan,
+            [f'bad url'])
+        return
+    await out.msg(self, modname, chan, [data.title.string])
 
 async def shorten(self, chan, msg, target = 'https://0x0.st/'):
     if len(msg) < 1:
@@ -38,10 +69,15 @@ async def unshorten(self, chan, src, msg):
     await out.msg(self, modname, chan, [f'{res}'])
 
 async def init(self):
+    self.handle_raw['url'] = filterurl
+
     self.cmd['0x0'] = shorten_0x0
     self.cmd['ttm'] = shorten_ttm
+    self.cmd['title'] = title
     self.cmd['unshorten'] = unshorten
 
     self.help['0x0'] = ['shorten [url] - shorten a url with 0x0.st']
     self.help['ttm'] = ['shorten [url] - shorten a url with ttm.sh']
     self.help['unshorten'] = ['unshorten [url] - try to unshorten a url']
+
+    self.url_re = re.compile(r'https?://\S+', re.I)

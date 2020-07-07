5b5e759cbf9f44a385a0f3f5e0452df2aec4ba23
Kiëd Llaentenn <kiedtl@tilde.team>
2020-07-07 10:27:50 -0400

    mod/karma.py: add

---
+++ mod/karma.py
@@ -0,0 +1,41 @@
+import common, dataset
+import out, random, re
+
+modname = 'karma'
+is_karma = re.compile('(^[+-]{2}\(?[\S\W]+\)?$)|(^\(?[\S\W]+\)?[+-]{2}$)')
+target_find = re.compile('([+-]{2})?\(?([^()+-]+)\)?([+-]{2})?')
+
+async def filterkarma(self, chan, src, msg):
+    if src == self.nickname:
+        return
+    if not is_karma.match(msg):
+        return
+    lo, target, ro = (target_find.findall(msg))[0]
+    op = lo or ro
+    entry = self.karmadb.find_one(name=target)
+    if entry == None:
+        karma = 0
+    else:
+        self.karmadb.delete(id = entry['id'])
+        karma = entry['amount']
+    if op == '++':
+        karma += 1
+    elif op == '--':
+        karma -= 1
+    self.karmadb.insert(dict(name=target, amount=karma))
+
+async def listkarma(self, chan, src, msg):
+    entry = self.karmadb.find_one(name=msg)
+    if entry == None:
+        await out.msg(self, modname, chan, [f'{msg} has 0 karma...'])
+    else:
+        num = entry['amount']
+        await out.msg(self, modname, chan, [f'{msg} has {num} karma!'])
+
+async def init(self):
+    self.handle_raw['filterkarma'] = filterkarma
+    self.karmadb = dataset.connect('sqlite:///dat/pnts.db')['karma']
+
+    self.cmd['karma'] = listkarma
+
+    self.help['karma'] = ['karma [thing] - get karma for thing. use <thing>++ or ++<thing> to set karma.']

f90dd6d0b2310955809b492873ba3bbda97f71c5
Kiëd Llaentenn <kiedtl@tilde.team>
2020-07-07 21:52:12 -0400

    youtube: add

---
+++ mod/youtube.py
@@ -0,0 +1,42 @@
+# youtube garbage stuff -_-
+
+import youtube as YT
+import common, out, re, secrets
+
+modname = 'youtube'
+is_yturl = re.compile(r'https?://\S+', re.I)
+youtube = YT.authenticate(secrets.yt_key)
+
+async def filteryt(self, chan, src, msg):
+    """
+    Detect a YT url in chat.
+    """
+    if self.yturl_re.match(msg):
+        yt_info(self, chan, src, msg)
+
+async def yt_info(self, chan, src, msg):
+    try:
+        txt = common.get_backlog_msg(self, chan, msg)[1]
+    except:
+        await out.msg(self, modname, chan,
+            [self.err_backlog_too_short])
+        return
+
+    try:
+        v_id = YT.id_from_url(txt)
+    except:
+        await out.msg(self, modname, chan, [f'bad url'])
+        return
+
+    info = YT.video_info(youtube, v_id)
+    info_fmted = YT.fmt_video_info(info)
+    await out.msg(self, modname, chan, [info_fmted])
+
+async def init(self):
+    # disabled, now that tildebot is a thing
+    #self.handle_raw['yturl'] = filteryt
+
+    self.cmd['youtube'] = yt_info
+    self.cmd['yt'] = yt_info
+
+    self.help['yt'] = ['yt [num] - display info for a youtube url [num] messages back']

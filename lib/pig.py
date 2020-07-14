e09eb0b448ef5bf23786362e76c5bf76a1ec9db5
Kiëd Llaentenn <kiedtl@tilde.team>
2020-07-13 20:46:23 -0400

    pig: move core logic into lib/pig.py

---
+++ lib/pig.py
@@ -0,0 +1,60 @@
+# pig latin implementation
+
+import random
+import re
+
+def _two_chars(s):
+    if len(s) < 2:
+        return s[0]
+    else:
+        return s[0] + s[1]
+
+def _has_upper(s):
+    has = False
+    for ch in s:
+        if (ch.isupper()):
+            has = True
+            break
+    return has
+
+def pigify(ms):
+    # filter out ZWNJ's, which is sometimes output
+    # by bots e.g. tildebot
+    ms = ms.replace('\u200c', '')
+
+    # split on every non-alphabetic character,
+    # keeping the delimiters so that they can be
+    # added back later
+    data = re.split(r'([!-/:-@\[-`{-~\ 0-9]+)', ms)
+
+    disyms = ['sh', 'gl', 'ch', 'ph', 'tr', 'br', 'fr',
+        'bl', 'gr', 'st', 'sl', 'cl', 'pl', 'fl']
+
+    # translation happens in-place
+    for k in range(len(data)):
+        i = data[k]
+
+        if len(i) == 0:
+            continue
+
+        # actual translation
+        if i[0] in ['a', 'e', 'i', 'o', 'u']:
+            data[k] = i + 'way'
+        elif _two_chars(i) in disyms:
+            data[k] = i[2:] + i[:2] + 'ay'
+        elif i.isalpha() == False:
+            data[k] = i
+        else:
+            data[k] = i[1:] + i[0] + 'ay'
+
+        # correct captalization
+        # e.g. "You've" => "ouYay'evay" => "Ouyay'evay"
+        if (_has_upper(data[k])):
+            data[k] = data[k].lower()
+            data[k] = data[k][:1].upper() + data[k][1:]
+
+    return ''.join(data)
+
+def pig_ascii():
+    return random.choice(['(･ั(00)･ั)', '(´·(oo)·`)', '(·(oo)·)',
+        '(v -(··)-v)', '(> (··) <)', '(° (··) °)'])
--- mod/pig.py
+++ mod/pig.py
@@ -1,64 +1,25 @@
 import common
-import out, random, re
+import out
+import pig
+import random
+import re
 
 modname = 'pig'
-vowels = ['a', 'e', 'i', 'o', 'u']
-
-def twoch(s):
-    if len(s) < 2:
-        return s[0]
-    else:
-        return s[0] + s[1]
-
-def hasupper(s):
-    has = False
-    for ch in s:
-        if (ch.isupper()):
-            has = True
-    return has
 
 async def pigify(self, c, n, m):
     ms = []
     try:
         ms = common.get_backlog_msg(self, c, m)
     except:
-        await out.msg(self, modname, c, [f'ymay acklogbay isway ootay ortshay!'])
+        await out.msg(self, modname, c,
+            [f'ymay acklogbay isway ootay ortshay!'])
         return
-    await out.msg(self, modname, c, [await pigtext(self, ms)])
 
-async def pigtext(self, ms):
-    # filter out ZWNJ's
-    ms[1] = ms[1].replace('\u200c', '')
-    data = re.split(r'([!-/:-@\[-`{-~\ 0-9]+)', ms[1])
+    pigtext = pig.pigify(ms[1])
+    pigface = pig.pig_ascii()
 
-    disyms = ['sh', 'gl', 'ch', 'ph', 'tr', 'br', 'fr',
-        'bl', 'gr', 'st', 'sl', 'cl', 'pl', 'fl']
-
-    for k in range(len(data)):
-        i = data[k]
-        if len(i) == 0:
-            continue
-
-        # translation
-        if i[0] in vowels:
-            data[k] = i + 'way'
-        elif twoch(i) in disyms:
-            data[k] = i[2:] + i[:2] + 'ay'
-        elif i.isalpha() == False:
-            data[k] = i
-        else:
-            data[k] = i[1:] + i[0] + 'ay'
-
-        # correct captalization
-        # e.g. "You've" => "ouYay'evay" => "Ouyay'evay"
-        if (hasupper(data[k])):
-            data[k] = data[k].lower()
-            data[k] = data[k][:1].upper() + data[k][1:]
-
-    pig = random.choice(['(･ั(00)･ั)', '(´·(oo)·`)', '(·(oo)·)',
-        '(v -(··)-v)', '(> (··) <)', '(° (··) °)'])
-    usr = common.nohighlight(ms[0])
-    return f'<{usr}> {"".join(data)} {pig}'
+    await out.msg(self, modname, c,
+        [f'<{ms[0]}> {pigtext} {pigface}'])
 
 async def init(self):
     self.handle_cmd['pig'] = pigify

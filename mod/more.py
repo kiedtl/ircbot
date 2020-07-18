b077f9f75eeb6e0b1f7a111de0ec29766ee01e94
Kiëd Llaentenn <kiedtl@tilde.team>
2020-07-18 15:13:10 -0400

    more: add

---
+++ mod/more.py
@@ -0,0 +1,18 @@
+import handlers
+import out
+
+modname = 'more'
+
+async def more(self, ch, src, msg, args, opts):
+    '''
+    :name: more
+    :hook: cmd
+    :help: view more text
+    :args:
+    :aliases: m
+    '''
+
+    await out.more(self, ch)
+
+async def init(self):
+    handlers.register(self, modname, more)

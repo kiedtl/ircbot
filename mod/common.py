23dbce60e68482bc5cb8d4e795dbe36860c6b936
Kiëd Llaentenn <kiedtl@tilde.team>
2020-07-19 18:05:07 -0400

    .gitignore: update

--- mod/common.py
+++ mod/common.py
@@ -32,14 +32,14 @@ def get_backlog_msg(self, chan, msg):
     """get message from backlog"""
 
     if len(msg) < 1:
-        msg = '0'
+        msg = '1'
     try:
         back = int(msg) + 0
     except:
         back = 1
 
     if chan in self.backlog and len(self.backlog[chan]) >= back:
-        return self.backlog[chan][0-back]
+        return self.backlog[chan][0 - back]
     else:
         raise BacklogTooShort('backlog too short')
 

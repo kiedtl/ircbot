22b2c5e603d7f5c8317e998907d357998fdf16fb
Kiëd Llaentenn <kiedtl@tilde.team>
2020-08-27 17:26:18 +0000

    lib/configuration.py: add

---
+++ lib/configuration.py
@@ -0,0 +1,10 @@
+# configuration for the bot.
+#
+# tables:
+#   defaults   ( key: string, value: string, type )
+#   #<channel> ( key: string, value: string, type )
+#   nickname   ( key: string, value: string, type )
+#
+# if the #<channel>'s value for a particular key is
+# not set, it defaults to the value provided in the
+# defaults table.

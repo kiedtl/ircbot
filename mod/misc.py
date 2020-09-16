cd2ae026e3c2f0a4cbb6135ece2864d8f12a8b57
Kiëd Llaentenn <kiedtl@tilde.team>
2020-09-16 21:10:50 +0000

    Revert "misc: update"
    This reverts commit 84f975eb860a6c65a2c42a1d9a68015d773c923f.

--- mod/misc.py
+++ mod/misc.py
@@ -2,13 +2,56 @@
 
 import config
 import common
+import handlers
 import out
+import os
 import random
 
 
 async def list_mods(self, chan, src, msg):
     mods = ", ".join(sorted(list(self.modules.keys())))
     await out.msg(self, "modules", chan, [f"loaded: {mods}"])
+
+
+async def load_mod(self, chan, src, msg, args, opts):
+    """
+    :name: loadmodule
+    :hook: cmd
+    :help: load an unloaded module from $ROOT/mod/
+    :args: module:str
+    :require_admin:
+    :aliases: load
+    """
+    mod = msg.split()[0]
+    mods = [s for s in os.listdir("mod") if ".py" in s]
+    if not f"{mod}.py" in mods:
+        await out.msg(self, "modules", chan, ["no such module"])
+        return
+
+    self.log("modules", f"loading {mod}")
+    m = __import__("mod." + mod)
+    m = eval("m." + mod)
+    await m.init(self)
+    self.modules[mod] = m
+    await out.msg(self, "modules", chan, ["loaded module"])
+
+
+async def unload_mod(self, chan, src, msg, args, opts):
+    """
+    :name: unloadmodule
+    :hook: cmd
+    :help: unload an loaded module
+    :args: module:str
+    :require_admin:
+    :aliases: unload
+    """
+    mod = msg.split()[0]
+    if not mod in self.modules:
+        await out.msg(self, "modules", chan, ["no such module"])
+        return
+    else:
+        del self.modules[mod]
+        await out.msg(self, "modules", chan, ["unloaded module"])
 
 
 async def ping(self, chan, src, msg):
@@ -19,18 +62,13 @@ async def ping(self, chan, src, msg):
 
 
 async def whoami(self, chan, src, msg):
-    response = ""
-
     owner = common.nohighlight(config.botmaster)
-    response += f"I'm {self.nickname}! | owner: {owner} "
-
-    if not config.upstream == None:
-        source = "".join([common.nohighlight(i) for i in config.upstream])
-        response += f"| source: {source} "
-
     email = common.nohighlight(config.email[0]) + "‍＠‍" + config.email[1]
-    response += f"| contact: {email} | usage: try {config.prefix}help"
-    await out.msg(self, "who", chan, [response])
+    source = "".join([common.nohighlight(i) for i in config.upstream])
+    await out.msg(self, "who", chan, [f"I'm {self.nickname}, {owner}'s bot."])
+    await out.msg(self, "who", chan, [f"upstream: {source}"])
+    await out.msg(self, "who", chan, [f"reporting issues: {email}"])
+    await out.msg(self, "who", chan, [f"for usage info, try {config.prefix}help"])
 
 
 async def init(self):
@@ -41,3 +79,6 @@ async def init(self):
     self.help["modules"] = ["modules - list loaded modules"]
     self.help["ping"] = ["ping - check if I'm responding"]
     self.help["who"] = ["who - get information about my owner"]
+
+    handlers.register(self, "modules", load_mod)
+    handlers.register(self, "modules", unload_mod)

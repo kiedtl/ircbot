83969b4c8c621ccf4736e7db39b29309f26c7d33
Kiëd Llaentenn <kiedtl@tilde.team>
2020-11-18 23:35:19 +0000

    admin.py: flatten admin module

--- mod/admin.py
+++ mod/admin.py
@@ -6,21 +6,29 @@
 import config
 import common, importlib, out, os, time
 import traceback
+import handlers
 import pprint
 import restart as _restart
 
 modname = "admin"
 
+async def _aexec(self, code):
+    # Make an async function with the code and `exec` it
+    exec(f"async def __ex(self): " + "".join(f"\n {l}" for l in code.split("\n")))
 
-async def dump(self, chan, source, msg):
-    """
-    dump the contents of self to a file
-    for debugging purposes.
-    """
-    if len(msg) < 1:
-        await out.msg(self, modname, chan, ["need filename"])
-        return
+    # Get `__ex` from local variables, call it and return the result
+    return await locals()["__ex"](self)
 
+
+async def dump(self, chan, source, msg, args, opts):
+    """
+    :name: coredump
+    :hook: cmd
+    :help: dump the bot's global variables to a file for debugging purposes.
+    :args: file:str
+    :require_admin:
+    :aliases:
+    """
     with open(msg, "w") as f:
         pprint.pprint(vars(self), stream=f)
         pprint.pprint("\n\n\n", stream=f)
@@ -28,20 +36,48 @@ async def dump(self, chan, source, msg):
     await out.msg(self, modname, chan, ["done"])
 
 
-async def quit(self, chan, source, msg):
-    await self.quit(config.quitmsg)
+async def quit(self, chan, source, msg, args, opts):
+    """
+    :name: quit
+    :hook: cmd
+    :help: shutdown the bot
+    :args: @msg:str
+    :require_admin:
+    :aliases:
+    """
+    quitmsg = config.quitmsg
+    if len(msg) > 1:
+        quitmsg = msg
+    await self.quit(quitmsg)
 
 
-async def restart(self, chan, source, msg):
+async def restart(self, chan, source, msg, args, opts):
+    """
+    :name: restart
+    :hook: cmd
+    :help: restart the bot
+    :args:
+    :require_admin:
+    :aliases:
+    """
     await self.quit(config.quitmsg)
     try:
         _restart.restart()
     except Exception as e:
         self.log(modname, "encountered fatal exception while restarting")
         self.log(modname, f"{repr(e)}")
+        exit(1)
 
 
-async def reloadmods(self, chan, source, msg):
+async def reloadmods(self, chan, source, msg, args, opts):
+    """
+    :name: reload
+    :hook: cmd
+    :help: reload the bot's modules
+    :args:
+    :require_admin:
+    :aliases: rl
+    """
     before = time.time()
     await out.msg(self, modname, chan, ["reloading modules..."])
 
@@ -86,111 +122,115 @@ async def reloadmods(self, chan, source, msg):
     )
 
 
-async def part(self, chan, source, msg):
+async def part(self, chan, source, msg, args, opts):
+    """
+    :name: part
+    :hook: cmd
+    :help: make bot leave a channel
+    :args: channel:str
+    :require_admin:
+    :aliases:
+    """
     await self.part(msg)
 
 
-async def join(self, chan, source, msg):
+async def join(self, chan, source, msg, args, opts):
+    """
+    :name: join
+    :hook: cmd
+    :help: make bot join a channel
+    :args: channel:str
+    :require_admin:
+    :aliases:
+    """
     await self.join(msg)
 
 
-async def joins(self, chan, source, msg):
+async def joinall(self, chan, source, msg, args, opts):
+    """
+    :name: joinall
+    :hook: cmd
+    :help: make bot join all channels listed in the bot's config
+    :args:
+    :require_admin:
+    :aliases: joins
+    """
     for i in config.prod_chans:
         await self.join(i)
 
 
-async def aexec(self, code):
-    # Make an async function with the code and `exec` it
-    exec(f"async def __ex(self): " + "".join(f"\n {l}" for l in code.split("\n")))
-
-    # Get `__ex` from local variables, call it and return the result
-    return await locals()["__ex"](self)
-
-
-async def ev(self, chan, source, msg):
+async def ev(self, chan, source, msg, args, opts):
+    """
+    :name: eval
+    :hook: cmd
+    :help: evaluate some Python code
+    :args: code:list
+    :require_admin:
+    :aliases: ev
+    """
     msg = msg.split(" ")
     try:
-        result = await aexec(self, " ".join(msg))
+        result = await _aexec(self, " ".join(msg))
     except Exception as e:
         await out.msg(self, modname, chan, [f"segmentation fault: {repr(e)}"])
         return
     await out.msg(self, modname, chan, [f"result: '{result}'"])
 
 
-async def send(self, c, n, m):
+async def send(self, c, n, m, args, opts):
+    """
+    :name: send
+    :hook: cmd
+    :help: send <text> to <channel>
+    :args: channel:str text:list
+    :require_admin:
+    :aliases:
+    """
     msg = m.split(" ")
     await self.message(msg.pop(0), " ".join(msg))
 
 
-async def shutup(self, c, n, m):
+async def shutup(self, c, n, m, args, opts):
+    """
+    :name: sleep
+    :hook: cmd
+    :help: disable the bot in the current channel for [minutes] (default is 5)
+    :args: @minutes:int
+    :require_admin:
+    :aliases:
+    """
     duration = 5
     if len(m) >= 1:
         try:
             duration = int(m) + 0
         except:
-            duration = 5
+            pass
     self.asleep[c] = time.time() + (duration * 60)
     await out.msg(self, modname, c, [f"disabled for {duration}m"])
 
 
-async def wake(self, c, n, m):
+async def wake(self, c, n, m, args, opts):
+    """
+    :name: wake
+    :hook: cmd
+    :help: enable the bot in the current channel
+    :args:
+    :require_admin:
+    :aliases:
+    """
     self.asleep[c] = time.time()
     await out.msg(self, modname, c, ["I'm back!"])
 
 
-commands = {
-    "coredump": dump,
-    "quit": quit,
-    "restart": restart,
-    "reload": reloadmods,
-    "part": part,
-    "join": join,
-    "eval": ev,
-    "send": send,
-    "joins": joins,
-    "sleep": shutup,
-    "wake": wake,
-}
-
-
-async def adminHandle(self, chan, source, msg):
-    if await self.is_admin(source):
-        msg = msg.split(" ")
-        if len(msg) < 1 or not msg[0] in commands:
-            await out.msg(self, modname, chan, [self.err_invalid_command])
-            return
-        print(
-            "{} recieved {} signal from {}".format(
-                common.modname("admin"), msg[0], source
-            )
-        )
-        await commands[msg.pop(0)](self, chan, source, " ".join(msg))
-    else:
-        await out.msg(self, modname, chan, ["insufficient privileges"])
-
-
 async def init(self):
-    self.handle_cmd["admin"] = adminHandle
-    self.aliases["admin"] = ["a"]
-
-    self.help["admin"] = [
-        "admin - various bot owner commands",
-        "admin subcommands: coredump quit restart reload part join joins eval send sleep wake",
-    ]
-    self.help["admin coredump"] = [
-        "admin coredump <file> - dump contents of self to file"
-    ]
-    self.help["admin quit"] = ["admin quit <message> - shutdown bot"]
-    self.help["admin restart"] = ["admin restart <message> - restart bot"]
-    self.help["admin reload"] = ["admin reload - reload modules and configs"]
-    self.help["admin part"] = ["admin part <channel> - make bot leave channel"]
-    self.help["admin join"] = ["admin join <channel> -  make bot join channel"]
-    self.help["admin joins"] = [
-        "admin joins - join channels defined in the admin module"
-    ]
-    self.help["admin eval"] = ["admin eval <command> - evaluate command"]
-    self.help["admin send"] = ["admin send <channel> <message> - send message"]
-    self.help["admin sleep"] = [
-        "admin sleep [num] - send me into an enchanted sleep for [num] minutes (default: 5m)"
-    ]
-    self.help["admin wake"] = ["admin wake - wake me up from an enchanted sleep"]
+    handlers.register(self, modname, dump)
+    handlers.register(self, modname, quit)
+    handlers.register(self, modname, restart)
+    handlers.register(self, modname, reloadmods)
+    handlers.register(self, modname, part)
+    handlers.register(self, modname, join)
+    handlers.register(self, modname, joinall)
+    handlers.register(self, modname, ev)
+    handlers.register(self, modname, send)
+    handlers.register(self, modname, shutup)
+    handlers.register(self, modname, wake)

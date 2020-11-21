33a7371e8383e3217baabedc20204f105e8b1d46
Kiëd Llaentenn <kiedtl@tilde.team>
2020-11-21 21:50:21 +0000

    birthday: allow for viewing other's birthday

--- mod/birthday.py
+++ mod/birthday.py
@@ -13,6 +13,15 @@ VALID_DATE = r"(\d{4}-\d{2}-\d{2}|\d{2}-(jan|feb|mar|apr|may|jun|jul|aug|sep|oct
 @manager.config("birthday", ConfigScope.USER, VALID_DATE, "YYYY-MM-DD or DD-MMM")
 async def birthday(self, chan, nick, msg):
     user = self.users[nick]['account']
+
+    args = msg.split()
+    if len(args) > 0:
+        if args[0] in self.users:
+            user = self.users[args[0]]['account']
+        else:
+            await self.msg(modname, chan, [f"I don't know who {args[0]} is."])
+            return
+
     birthday_str = configuration.get(self.network, user, "birthday")
     today = datetime.datetime.today()
 

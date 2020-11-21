db948a02aff138b833402074f47b1ffb0aec21e5
Kiëd Llaentenn <kiedtl@tilde.team>
2020-11-21 20:00:40 +0000

    config.py: update

--- lib/config.py
+++ lib/config.py
@@ -12,34 +12,19 @@ tls = False
 tls_verify = False
 
 join_on_invite = True
-respond_to_rollcall = True
 set_botmode = True
 
 prefix = ":"
-admins = [
-    "kiedtl",
-    "spacehare",
-    "ben",
-    "cmccabe",
-    "gbmor",
-    "tomasino",
-    "ubergeek",
-    "deepend",
-    "calamitous",
-]
+admins = ["kiedtl", "spacehare"]
 
-bannedchans = ["#meta", "##jmw2020", "#communism"]
+bannedchans = ["#meta", "##jmw2020", "#communism", '#chaos']
 
 # channels that are joined only when the
 # `:admin joins` command is issued
 prod_chans = [
     "#team",
-    "#lickthecheese",
-    "#chaos",
-    "#unruly",
-    "#koth",
-    "#ricing",
     "#lounge",
+    "#xfnw",
 ]
 
 # channels that are joined initially

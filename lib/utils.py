1746efbba237e29847c2fd72e12ffedc974bdd21
Kiëd Llaentenn <kiedtl@tilde.team>
2020-11-24 12:57:56 +0000

    bot: update

--- lib/utils.py
+++ lib/utils.py
@@ -7,3 +7,15 @@ def flatten(src):
     Flatten a list of lists
     """
     return [item for sublist in src for item in sublist]
+
+
+def dedup(src):
+    """
+    Dedup an array.
+    TODO: use binary search to speed this thing up...?
+    """
+    dest = []
+    for i in src:
+        if i not in dest:
+            dest.append(i)
+    return dest

d659f404cac72100e3b40c5f3e300928b6308a75
Kiëd Llaentenn <kiedtl@tilde.team>
2021-04-07 14:02:44 +0000

    mod/sed.py: liberal matching when moving through backlog, so subsequent edits to a sed expr don't edit the previous sed expr

--- mod/sed.py
+++ mod/sed.py
@@ -8,8 +8,9 @@ from handlers import *
 
 modname = "text"
 
-IS_SED = r"(?:(?P<user>[a-zA-Z0-9_\-\\\[\]\{\}^`|]+): )?(?P<sed>s/[^/]+/[^/]*/)(?P<seds> s/[^/]+/[^/]*/)*"
+IS_SED = r"^(?:(?P<user>[a-zA-Z0-9_\-\\\[\]\{\}^`|]+): )?(?P<sed>s/[^/]+/[^/]*/)(?P<seds> s/[^/]+/[^/]*/)*"
 IS_SED_comp = re.compile(IS_SED)
+IS_SED_LIBERAL = re.compile(r"^(([a-zA-Z0-9_\-\\\[\]\{\}^`|]+): )?(s/[^/]+/[^/]*/?)")
 
 
 def _sed(e, m):
@@ -38,7 +39,7 @@ async def filtersed(self, chan, src, msg):
         backlog = list(reversed(self.backlog[chan]))
         for back_msg in backlog:
             if back_msg[0] == user:
-                if IS_SED_comp.match(back_msg[1]):
+                if IS_SED_LIBERAL.match(back_msg[1]):
                     continue
                 if _sed(sections["sed"], back_msg[1]) is sedinput:
                     continue
